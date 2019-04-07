87484946643e0751e6329d0ed9f20f23241fec28
夜禹 <mingyu.ymy@alipay.com>
2019-04-07 18:43:27 +0800

    init

---
+++ src/main/java/com/yeye/springbootantdpro/SpringbootAntdproApplication.java
@@ -0,0 +1,13 @@
+package com.yeye.springbootantdpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAntdproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAntdproApplication.class, args);
+	}
+
+}
